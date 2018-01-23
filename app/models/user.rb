4e20cc8f4e8ff4bc913d394ebaf74fcdcd83807f
priyanka yadav <priyanka216668@gmail.com>
2018-01-23 13:00:03 +0530

    user model validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+	validates :name, presence: true, format:{with: /\A[a-zA-z]+ [a-zA-z]+\z/, message: 'Invalid Name' }
+	validates :email, presence: true, uniqueness: true, format: {with:/\A[A-Za-z0-9\W]+*@[A-Za-z0-9\W]+[.][a-z]+\z/, message: 'Invalid Email' } 
+	validates :password, presence: true, format: {with: /(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*\W+)/, message: 'Invalid Password' }
+	validates :gender, presence: true, format: {with: /\AMale\z|\AFemale\z/, message: 'gender can be Male or  Female ' }
+    validates :dob, presence: true
+	
 end
